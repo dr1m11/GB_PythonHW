ccc278441a6b4c4a602ea9342d8b650804a4faae
Marat <marat3389@gmail.com>
2023-05-02 11:30:06 +0300

    exercise 1 completed

--- HW5/Ex1/main.py
+++ HW5/Ex1/main.py
@@ -1,10 +1,17 @@
 #  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
 
+first = int(input())
+second = int(input())
+
 def step(a, b):
-    c = 1
-    c *= a
-    if a != a ** b:
-        return step(c, b)
+    c = a ** b
+    p = a
+    def step2(a):
+        if a == c:
+            return a
+        return step2(a * p)
+    return step2(a)
+    
 
 
-print(step(2, 3))
+print(step(first, second))
